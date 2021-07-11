43ac535830690e7b4c82e04e51c2d1c73642c14d
BlaChalk <black00731@gmail.com>
2021-07-11 10:26:09 +0800

    + weather API
    使用中央氣象局開放資料平臺之資料擷取API：
    https://opendata.cwb.gov.tw/dist/opendata-swagger.html#/%E9%A0%90%E5%A0%B1/get_v1_rest_datastore_F_C0032_001

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template lang="pug">
 #app
-  h1 hello
   //- <img alt="Vue logo" src="./assets/logo.png">
   //- <HelloWorld msg="Welcome to Your Vue.js App"/>
+  h1 氣象據報觀測資料
+  cityList(v-for="weatherData in weatherDatas") 
+    city {{ weatherData.locationName }}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      weatherDatas: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-262367AE-AD07-4CEB-88B6-3D6237F6A839&format=JSON&locationName=')
+      .then(response => {
+      // console.log(response.data)
+      this.weatherDatas = response.data.records.location
+    })
   }
 }
 </script>
