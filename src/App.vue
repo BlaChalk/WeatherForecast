fe61dcd026561d3e5d3b1cb9cddd85a1e401a2d5
BlaChalk <black00731@gmail.com>
2021-07-19 17:04:13 +0800

    change weather API
    改為使用臺灣各縣市鄉鎮未來1週逐12小時天氣預報

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template lang="pug">
 #app
-  //- <img alt="Vue logo" src="./assets/logo.png">
-  //- <HelloWorld msg="Welcome to Your Vue.js App"/>
-  h1 氣象據報觀測資料
-  cityList(v-for="weatherData in weatherDatas") 
-    city {{ weatherData.locationName }}
+  h1 氣象預報觀測資料
+  .cityList(v-for="weatherData in weatherDatas") {{ weatherData.locationName }} 
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data() {
     return {
@@ -22,12 +16,28 @@ export default {
     }
   },
   mounted () {
+    // 臺灣各縣市鄉鎮未來1週逐12小時天氣預報
     axios
-      .get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-262367AE-AD07-4CEB-88B6-3D6237F6A839&format=JSON&locationName=')
-      .then(response => {
+    .get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-093?Authorization=CWB-262367AE-AD07-4CEB-88B6-3D6237F6A839&locationId=F-D0047-091')
+    .then(response => {
       // console.log(response.data)
-      this.weatherDatas = response.data.records.location
+      this.weatherDatas = response.data.records.locations[0].location
     })
+    .catch((error) => {
+      if (error.response) {
+        // When response status code is out of 2xx range 
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      } else if (error.request) {
+        // When no response was recieved after request was made
+        console.log(error.request)
+      } else {
+        // Error
+        console.log(error.message)
+      }
+    })
+    
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
 // import './plugins/axios'
-import axios from 'axios'
 
 createApp(App).use(store).mount('#app')
 
