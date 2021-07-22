598de99e92903780ba2860628800eef6916e99d3
BlaChalk <black00731@gmail.com>
2021-07-22 15:04:26 +0800

    + weather-status & week temperature @WeatherBox
    using fake data
    + high & low temperature

--- src/components/WeatherBox.vue
+++ src/components/WeatherBox.vue
@@ -3,6 +3,7 @@
   .cityList(v-for="weatherData in weatherDatas" v-show="weatherData.locationName === '臺中市'")
     .top
       .background
+      h3.weather-status {{ weatherData.weatherElement[6].time[0].elementValue[0].value }}
       .weather-measures
         .UVI 紫外線指數：{{ weatherData.weatherElement[9].time[0].elementValue[0].value }}
         .RH 平均濕度：{{ weatherData.weatherElement[2].time[0].elementValue[0].value }}%
@@ -18,6 +19,9 @@
         h4 MON
         svg(viewBox="-50 -50 100 100")
           circle.sun(cx=0 cy=0 r=25)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 TUE
         svg(viewBox="-60 -50 100 120")
@@ -27,6 +31,9 @@
           circle.cloud(cx=-30 cy=30 r=20)
           circle.cloud(cx=-20 cy=10 r=15)
           circle.cloud(cx=-7 cy=15 r=15)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 WED
         svg(viewBox="-50 -20 100 100")
@@ -38,22 +45,37 @@
           circle.cloud(cx=-20 cy=30 r=20)
           circle.cloud(cx=-10 cy=10 r=15)
           circle.cloud(cx=-3 cy=15 r=15)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 THU
         svg(viewBox="-50 -50 100 100")
           circle.sun(cx=0 cy=0 r=25)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 FRI
         svg(viewBox="-50 -50 100 100")
           circle.sun(cx=0 cy=0 r=25)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 SAT
         svg(viewBox="-50 -50 100 100")
           circle.sun(cx=0 cy=0 r=25)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
       .dayweather
         h4 SUN
         svg(viewBox="-50 -50 100 100")
           circle.sun(cx=0 cy=0 r=25)
+        .temperature
+          h5.high 32°C
+          h5.low 28°C
 
           
 </template>
@@ -112,6 +134,8 @@ $font_size: 18px
 $color_white: #fdfbfc
 $color_gray: #ccc
 $coler_yellow: #FFD633
+$color_dark_red: #C1666B
+$color_dark_blue: #4281A4
 $color_rain: #4DACFF
 
 *
@@ -129,14 +153,19 @@ $color_rain: #4DACFF
 
   &:hover
     .top
-      height: 450px
+      height: 430px
       .background
         top: 30px
+      .weather-status
+        opacity: 0
       .weather-measures
         opacity: 1
     .bottom
-      height: 75px
+      height: 95px
       opacity: 1
+      .dayweather
+        .temperature
+          opacity: 1
 
 .top
   height: 400px
@@ -147,6 +176,12 @@ $color_rain: #4DACFF
     background-image: url('~@/assets/extra_large.jpg')
     background-size: cover
     transition: 1s
+  .weather-status
+    position: absolute
+    right: 20px
+    top: 5px
+    opacity: 1
+    transition: 0.5s
   .weather-measures
     display: flex
     flex-direction: column
@@ -191,44 +226,56 @@ $color_rain: #4DACFF
     margin: 0px 10px 0px 10px
     align-items: center
     h4
-      margin: 5px 0px 10px 0px
+      margin: 5px 0px 5px 0px
     svg
-        width: 100%
-        max-width: 40px
-        height: 100px
-        .sun
-          fill: rgba($coler_yellow, 0.9)
-          animation: sun-breath 3s infinite
-        .cloud
-          fill: $color_gray
-          animation: cloud-drift 4s infinite
-        .rain
-          stroke: rgba($color_rain, 0.8)
-          animation: rain-drop 1.5s infinite linear
-        .rain2
-          animation-delay: -0.7s
-        .rain3
-          animation-delay: -0.3s
-        @keyframes sun-breath
-          0%
-            transform: scale(0.8)
-          50%
-            transform: scale(1)
-          100%
-            transform: scale(0.8)
-        @keyframes cloud-drift
-          0%
-            transform: translateY(0px)
-          50%
-            transform: translateY(-10px)
-          100%
-            transform: translateY(0px)
-        @keyframes rain-drop
-          0%
-            transform: translateY(0px)
-          85%
-            opacity: 1
-          100%
-            transform: translateY(45px)
-            opacity: 0
+      width: 100%
+      max-width: 40px
+      height: 40px
+      .sun
+        fill: rgba($coler_yellow, 0.9)
+        animation: sun-breath 3s infinite
+      .cloud
+        fill: $color_gray
+        animation: cloud-drift 4s infinite
+      .rain
+        stroke: rgba($color_rain, 0.8)
+        animation: rain-drop 1.5s infinite linear
+      .rain2
+        animation-delay: -0.7s
+      .rain3
+        animation-delay: -0.3s
+      @keyframes sun-breath
+        0%
+          transform: scale(0.8)
+        50%
+          transform: scale(1)
+        100%
+          transform: scale(0.8)
+      @keyframes cloud-drift
+        0%
+          transform: translateY(0px)
+        50%
+          transform: translateY(-10px)
+        100%
+          transform: translateY(0px)
+      @keyframes rain-drop
+        0%
+          transform: translateY(0px)
+        85%
+          opacity: 1
+        100%
+          transform: translateY(45px)
+          opacity: 0
+    .temperature
+      display: flex
+      opacity: 0
+      transition: 0.5s
+      tras
+      h5  
+        margin: 2px
+      .high
+        color: rgba($color_dark_red, 0.8)
+      .low
+        color: rgba($color_dark_blue, 0.6)
+
 </style>
