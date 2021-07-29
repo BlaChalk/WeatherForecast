233de0439a041d7e3a96a3eb9335b8658730f7ef
BlaChalk <black00731@gmail.com>
2021-07-29 08:58:25 +0800

    + pages index @vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/WeatherForecast/'
-    : '/'
+    : '/',
+    pages: {
+      index: {
+         // entry for the page
+        entry: 'src/main.js',
+        // the source template
+        template: 'public/index.html',
+        // output as dist/index.html
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: '台灣氣象觀測站'
+      }
+    }
 }
