7c350cf974dc48365407569363d5f16a5a25c7a4
BlaChalk <black00731@gmail.com>
2021-08-10 13:22:22 +0800

    deploy

---
+++ precache-manifest.024502a4be9d81949d8fd0b5ea9065c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b00c17044e70052686",
+    "url": "/WeatherForecast/css/chunk-vendors.6680f48e.css"
+  },
+  {
+    "revision": "b3f44a6fb43ea74cb9e3",
+    "url": "/WeatherForecast/css/index.1a5e9a1f.css"
+  },
+  {
+    "revision": "bd96698ea63fd9e96923c2ec12a7e87b",
+    "url": "/WeatherForecast/img/extra_large.bd96698e.jpg"
+  },
+  {
+    "revision": "f6aadffb5a9ddd79fe7d480449a2ba9a",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "f1b00c17044e70052686",
+    "url": "/WeatherForecast/js/chunk-vendors.6beb3d97.js"
+  },
+  {
+    "revision": "b3f44a6fb43ea74cb9e3",
+    "url": "/WeatherForecast/js/index.1f2856ee.js"
+  },
+  {
+    "revision": "5acdd9479cf02457e8edc9affec82c99",
+    "url": "/WeatherForecast/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/WeatherForecast/robots.txt"
+  }
+]);
