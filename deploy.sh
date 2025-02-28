e563da5f31f0fa585de11f5dc8c0b8d1421d2f40
BlaChalk <black00731@gmail.com>
2025-02-28 14:35:40 +0800

    adjust build script and add echo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@
 # 當發生錯誤時終止腳本運行
 set -e
 
+echo "Run deploy.sh"
 # 打包
-npm run build
+yarn build
 
 # 移動至到打包後的dist目錄 
 cd dist
@@ -14,8 +15,10 @@ git init #因為dist資料夾預設是被ignore的，因此在進入dist資料�
 git add -A
 git commit -m 'deploy'
 
+echo "Push to Github"
 # 部署到 https://github.com/BlaChalk/WeatherForecast.git 分支為 gh-pages
 git push -f https://github.com/BlaChalk/WeatherForecast.git master:gh-pages
 #將dist資料夾中的內容推送至遠端的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+echo "Deploy Successfully!"
 
 cd ..
